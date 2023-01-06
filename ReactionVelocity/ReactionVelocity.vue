0c147dc699f5041855681dff08ec8b1f90c8139a
dbshin <dbshin@daontec.kr>
2023-01-06 17:27:00 +0900

    [eoqna] 23-01-06 17:26 Lotto까지 학습

--- ReactionVelocity/ReactionVelocity.vue
+++ ReactionVelocity/ReactionVelocity.vue
@@ -1,44 +1,73 @@
 <template>
     <div>
-        <div id="screen" v-bind:class="state">{{msg}}</div>
-        <div>
-            <div>평균 시간 : {{}}</div>
+        <div id="screen" :class="state" @click="onClickScreen">{{msg}}</div>
+        <template v-if="result.length">
+            <div>평균 시간 : {{average}}ms</div>
             <button @click="onReset">리셋</button>
-        </div>
+        </template>
     </div>
 </template>
 
 <script>
+    let startTime = 0;
+    let endTime = 0;
+    let timeout = null;
+
     export default {
         data() {
             return {
                 result: [],
                 state: 'waiting',
                 msg: '클릭해서 시작하세요.',
+            };
+        },
+        computed: {
+            average() {
+                return this.result.reduce((a, c) => a + c, 0) / this.result.length || 0;
             }
         },
         methods: {
             onReset() {
-
+                this.result = [];
             },
-        },
+            onClickScreen() {
+                if(this.state === 'waiting') {
+                    this.state = 'ready';
+                    this.msg = '초록색이 되면 클릭하세요.';
+                    timeout = setTimeout(() => {
+                        this.state = 'now';
+                        this.msg = '지금 클릭!';
+                        startTime = new Date();
+                    }, Math.floor(Math.random() * 1000) + 2000); //2~3초
+                } else if(this.state === 'ready') {
+                    clearTimeout(timeout);
+                    this.state = 'waiting';
+                    this.msg = '너무 성급하시군요! 초록색이 된 후에 클릭하세요.'
+                } else if(this.state === 'now') {
+                    endTime = new Date();
+                    this.state = 'waiting';
+                    this.msg = '클릭해서 시작하세요.'
+                    this.result.push(endTime - startTime);
+                }
+            },
+         },
     };
 </script>
 
-<style>
-#screen {
-    width: 300px;
-    height: 200px;
-    text-align: center;
-    user-select: none;
-}
-#screen.waiting {
-    background-color: aqua;
-}
-#screen.ready {
-    background-color: red;
-}
-#screen.now {
-    background-color: greenyellow;
-}
+<style scoped>
+    #screen {
+        width: 300px;
+        height: 200px;
+        text-align: center;
+        user-select: none;
+    }
+    #screen.waiting {
+        background-color: aqua;
+    }
+    #screen.ready {
+        background-color: red;
+    }
+    #screen.now {
+        background-color: greenyellow;
+    }
 </style>
