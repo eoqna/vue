d18d671051634dc915ce9f4ae3600b96609372a5
dbshin <dbshin@daontec.kr>
2023-01-06 11:05:42 +0900

    [eoqna] 23-01-06 - 1.gitIgnore추가, 숫자야구 완성

---
+++ ReactionVelocity/ReactionVelocity.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div id="screen">{{msg }}</div>
+        <div>
+            <div>평균 시간 : {{ }}</div>
+            <button @click="onReset">리셋</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                result: [],
+                state: 'waiting',
+                msg: '클릭해서 시작하세요.',
+            }
+        },
+        methods: {
+            onReset() {
+
+            },
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
