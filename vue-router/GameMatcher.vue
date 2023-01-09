810a1370f031042f7043e5949bafdc95dfe2f79a
eoqna <eoqna@naver.com>
2023-01-09 22:26:23 +0900

    [eoqna] 23-01-09 22:26 vue-router 완강

---
+++ vue-router/GameMatcher.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-if="currentGame === 'number-baseball'">
+        <number-baseball></number-baseball>
+    </div>
+    <div v-else-if="currentGame === 'reaction-velocity'">
+        <reaction-velocity></reaction-velocity>
+    </div>
+    <div v-else-if="currentGame === 'rock-scissors-paper'">
+        <rock-scissors-paper></rock-scissors-paper>
+    </div>
+    <div v-else-if="currentGame === 'lotto-generator'">
+        <lotto-generator></lotto-generator>
+    </div>
+    <div v-else>일치하는 게임이 없습니다.</div>
+</template>
+
+<script>
+import NumberBaseball from '../NumberBaseball/NumberBaseball';
+import ReactionVelocity from '../ReactionVelocity/ReactionVelocity';
+import RockScissorsPaper from '../RSP/RockScissorsPaper';
+import LottoGenerator from '../Lotto/LottoGenerator';
+
+    export default {
+        components: {
+            NumberBaseball,
+            ReactionVelocity,
+            RockScissorsPaper,
+            LottoGenerator,
+        },
+        mounted() {
+            console.log(this.$router);
+            console.log(this.$route);
+        },
+        computed: {
+            currentGame() {
+                return this.$route.params.name;
+            }
+        }
+    }
+</script>
+
--- vue-router/Router.vue
+++ vue-router/Router.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <div>
-            <router-link to="/number-baseball">숫자야구</router-link>
-            <router-link to="/response-check">반응속도</router-link>
-            <router-link to="/rsp">가위바위보</router-link>
-            <router-link to="/lotto-generator">로또</router-link>
+            <router-link to="/game/index">게임 매쳐</router-link>
+            <router-link to="/game/number-baseball">숫자야구</router-link>
+            <router-link to="/game/reaction-velocity">반응속도</router-link>
+            <router-link to="/game/rock-scissors-paper">가위바위보</router-link>
+            <router-link to="/game/lotto-generator">로또</router-link>
         </div>
         <router-view></router-view>
     </div>
